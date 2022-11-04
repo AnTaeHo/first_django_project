f8a6a1c5c0abb108d9003e7dde84b777b245a6f0
antaeho <an981022@naver.com>
2022-11-04 21:19:35 +0900

    django course 21 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
+from django.views.generic import CreateView
+
 from accountapp.models import HelloWolrd
 
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 
 # Create your views here.
@@ -22,3 +26,10 @@ def hello_world(request):
     else:
         hello_world_list = HelloWolrd.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
